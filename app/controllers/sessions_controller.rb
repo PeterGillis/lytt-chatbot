fe65df90fa492e20df8b36132484074a4e5b9489
Peter Gillis <petergillis@hotmail.com>
2019-08-18 18:59:22 +0200

    successful test of basic routes using sample JSON

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,23 @@
 class SessionsController < ApplicationController
 
   def index
-    @sessions = sessions.all
+    @session = Session.all
+    # the following is test JSON only!
+
+    render json: {
+    "text": "{user-text-input}",
+    "identifier": "{unique-generated-id}"
+
+    }
+  end
+
+  def show
+    # @session = Session.find(params[:id])
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit
   end
 end
