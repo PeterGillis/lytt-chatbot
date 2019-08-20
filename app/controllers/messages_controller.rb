e0238c2e976b6cbf17df446ae620fbb4d12ee85b
Peter Gillis <petergillis@hotmail.com>
2019-08-20 13:07:51 +0200

    initial draft of JSON resposnes working

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,17 +15,38 @@ class MessagesController < ApplicationController
     # @session = Session.new
     # 1. take the session id from the query
     sessionid = params[:session_id]
-    # 2. acces the body and take text and identifier
+    # 2. access the body and take text and identifier
     body = JSON.parse request.body.read
     text = body[:text]
-    puts text
+    identifier = body[:identifier]
     # identifier
     # 3. If no text or no identifier respond with a 422 error
+    if params[:text] && [:identifier]
+      render json:
+      {
+        "message": {
+          "identifier": "{unique-generated-id}",
+          "detected_language": "es",
+          "timestamp": "2020-08-01T18:20:00Z"
+        }
+      }
+    else
+      render json:
+      {
+         "error": {
+           "code": 422,
+           "message": "Unfortunately we don't have support for your language yet."
+        }
+      }
+
+    end
     # 4. Check the language of the message
+
     # 5. Check if a session with that id already exists
     # 6a. If not: create a new one
-    # 6b. If exists: check whether the message language is the same of the
-    #    session language. If not: answer with a 422 error
+    # 6b. If exists: check whether the message language is the same as the
+    #    session language.
+    # 6c. If not: answer with a 422 error
     # 7. Save the message
     # 8. Respond with 201 and with the correct message
   end
